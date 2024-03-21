fec494d54c22559decbeff044869beec4c69dcdf
steedude <85280987+steedude@users.noreply.github.com>
2024-03-21 14:24:07 +0800

    feat: page LikedPosts

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -6,4 +6,5 @@ export { default as Profile } from "./Profile";
 export { default as UpdateProfile } from "./UpdateProfile";
 export { default as EditPost } from "./EditPost";
 export { default as PostDetails } from "./PostDetails";
+export { default as LikedPosts } from "./LikedPosts";
 export { default as AllUsers } from "./AllUsers";
