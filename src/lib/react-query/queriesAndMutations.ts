e0317424d533793eb8c4dfb0c57ada47d4701c60
steedude <85280987+steedude@users.noreply.github.com>
2024-03-22 16:35:54 +0800

    feat: savePost and likePost

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -13,6 +13,7 @@ import {
   likePost,
   savePost,
   deleteSavedPost,
+  getCurrentUser,
 } from "../appwrite/api";
 import { QUERY_KEYS } from "@/lib/react-query/queryKeys.ts";
 import { INewPost, INewUser } from "@/types";
@@ -116,5 +117,12 @@ export const useDeleteSavedPost = () => {
         queryKey: [QUERY_KEYS.GET_CURRENT_USER],
       });
     },
+  });
+};
+
+export const useGetCurrentUser = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+    queryFn: getCurrentUser,
   });
 };
